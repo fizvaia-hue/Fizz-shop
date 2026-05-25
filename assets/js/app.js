e1aab3438eb75a297b58ae36e6778073a0de0681
fizvaia-hue <fizvaia@gmail.com>
2026-05-26 03:14:52 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
 console.log("Fizz Shop Loaded Successfully");
+document
+.querySelector(".close-announcement")
+.addEventListener("click",()=>{
+document.querySelector(".announcement-bar").style.display="none";
+});
